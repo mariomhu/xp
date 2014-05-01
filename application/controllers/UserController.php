cadaf34e1d23454327ded66ea3c3d0da4717ef11
iagouilian <iagouilian@gmail.com>
2014-05-01 10:56:24 -0300

    User list, register, edit, remove e profile estão prontos

--- application/controllers/UserController.php
+++ application/controllers/UserController.php
@@ -1,9 +1,26 @@
 <?php
 
 class UserController extends Zend_Controller_Action {
-	
+
+
 	public function indexAction() {
 	
+		$login = $this->getParam("login");
+		if(!$login){
+			Application_Model_Auth::checkIsUser();
+			$user = Application_Model_Auth::getUser();
+			$login = $user["login"];
+		}
+	
+		$user = Application_Model_UserManager::getByLogin($login);
+	
+		$this->view->submissions = $user['ac']+ $user['pe'] + $user['wa'] + $user['ce'] + $user['re'] + $user['tl'];
+		$this->view->user = $user;
+	
+	}
+	
+	public function listAction() {
+
 		$this->view->users = Application_Model_UserManager::getAll(null, 'name');
 		
 	}
@@ -24,29 +41,67 @@ class UserController extends Zend_Controller_Action {
 				
 			}else if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
 				
+				$_POST['password'] = md5($_POST['password']);
 				Application_Model_UserManager::add($_POST);
 				$id = Zend_Db_Table::getDefaultAdapter()->lastInsertId();
 				$user = Application_Model_UserManager::get($id);
 				Application_Model_Auth::login($user);
-				$this->_helper->redirector("profile", "user");
+				$this->view->success = "Account successfully created.";
 				
 			}
 			
 		}
 	}
 	
-	public function profileAction() {
-		$login = $this->getParam("login");
-		if(!$login){
-			Application_Model_Auth::checkIsUser();
-			$user = Application_Model_Auth::getUser();
-			$login = $user["login"];
+	public function editAction() {
+	
+		Application_Model_Auth::checkIsUser();
+		if($this->getParam('id')){
+			Application_Model_Auth::checkIsAdmin();
+			$id = $this->getParam('id');
+			$admin = true;
+		}else{
+			$id = Application_Model_Auth::getUserId();
 		}
 		
-		$user = Application_Model_UserManager::getByLogin($login);
+		$user = Application_Model_UserManager::get($id);
 		
-		$this->view->submissions = $user['ac']+ $user['pe'] + $user['wa'] + $user['ce'] + $user['re'] + $user['tl'];
-		$this->view->user = $user;
+		if($_POST){
+				
+			if(Application_Model_UserManager::get(array("email = ?" => $_POST['email'], "id != ?" => $id))){
+	
+				$this->view->error = "This email is already registered.";
+	
+			}else if(!$admin && md5($_POST['current-password']) != $user['password']){
+	
+				$this->view->error = "Password incorrect.";
+	
+			}else if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+				
+				if(!$_POST['password']) unset($_POST['password']);
+				else $_POST['password'] = md5($_POST['password']);
+				
+				Application_Model_UserManager::set($_POST, $id);
+				$user = Application_Model_UserManager::get($id);
+				if(!$admin) Application_Model_Auth::login($user);
+				
+				$this->view->success = "Account successfully changed.";
+	
+			}
+				
+		}else{
+			$_POST = $user;
+		}
+	}
+	
+	public function removeAction(){
+		
+		$this->_helper->layout()->disableLayout();
+		$this->_helper->viewRenderer->setNoRender(true);
+		Application_Model_Auth::checkIsAdmin();
+		$id = $this->getParam('id');
+		Application_Model_UserManager::remove($id);
+		$this->_helper->redirector('index', 'user');
 		
 	}
 	
