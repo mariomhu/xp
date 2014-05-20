0b350b6a6ae1d288474def199124751c69a64792
Roberto <roberto.wtr@gmail.com>
2014-05-19 21:19:03 -0300

    Contest

--- application/controllers/ContestController.php
+++ application/controllers/ContestController.php
@@ -3,6 +3,15 @@
 class ContestController extends Zend_Controller_Action {
 
 	public function indexAction() {
+		$id = $this->getParam ( "id" );
+		
+		$problem = Application_Model_::get( $id );
+		
+		$this->view->submissions = $problem ['ac'] + $problem ['pe'] + $problem ['wa'] + $problem ['ce'] + $problem ['re'] + $problem ['tl'];
+		$this->view->problem = $problem;
+		
+		$this->view->tags = Application_Model_TagManager::getByProblem($id);
+		
 	
 	}
 	
--- application/models/ContestManager.php
+++ application/models/ContestManager.php
@@ -28,10 +28,10 @@ class Application_Model_ContestManager extends Application_Model_Manager{
 		), $id);
 	}
 	
-	public static function getByTag($tag){
-		$tag = intval($tag);
+	public static function getById($id){
+		$id = intval($id);
 		$select = self::select ();
-		$select->joinInner ( "problemtag", "problem.id = problemtag.problem and problemtag.tag = $tag" );
+		$select->joinInner ( "id", "contest.id = problemtag.problem and problemtag.tag = $tag" );
 		$db = Zend_Db_Table::getDefaultAdapter ();
 		return $db->query ( $select )->fetchAll ();
 	}
