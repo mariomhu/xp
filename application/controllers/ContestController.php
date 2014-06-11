4c0e4d34a5bf32e3731effa59739be5a79a3f529
Roberto <roberto.wtr@gmail.com>
2014-06-11 20:44:54 -0300

    Contest

--- application/controllers/ContestController.php
+++ application/controllers/ContestController.php
@@ -16,28 +16,18 @@ class ContestController extends Zend_Controller_Action {
 	}
 	
 	public function registerAction() {
-		Application_Model_Auth::checkIsAdmin ();
-		$this->view->contest = Application_Model_ContestManager::getAll(null, 'title');
+		//Application_Model_Auth::checkIsAdmin ();
 		if ($_POST) {
 				
-			//$_POST ['active'] = ! ! $_POST ['active'];
 			$_POST ['id'] = array_flip ( explode ( ',', $_POST ['id'] ) );
-				
+			$user = Application_Model_Auth::getUser();
+			$_POST ['admin'] = $user['id'];
+									
 			Application_Model_ContestManager::add ( $_POST );
-			/*$id = Zend_Db_Table::getDefaultAdapter ()->lastInsertId ();
-			//move_uploaded_file ( $_FILES ['description'] ['tmp_name'], "../data/problems/" . $id . ".pdf" );
-			//move_uploaded_file ( $_FILES ['cases'] ['tmp_name'], "../data/testcases/" . $id . ".zip" );
-				
-			foreach ( $_POST ['title'] as $key => $value ) {
-				if ($key)
-					Application_Model_ContestManager::add ( array (
-							'contest' => $id,
-							'tag' => $key
-					) );
-			}
-				
-			$this->view->id = $id;*/
-			$this->view->success = "Problem successfully created.";
+			$id = Zend_Db_Table::getDefaultAdapter ()->lastInsertId ();
+										
+			$this->view->id = $id;
+			$this->view->success = "Contest successfully created.";
 		}		
 	}
 	
